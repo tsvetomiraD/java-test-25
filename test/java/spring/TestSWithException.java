2d670dab9e1907dba26e68d6c1eea67dcb961d24
tsvetomiraD <95402102+tsvetomiraD@users.noreply.github.com>
2023-01-16 10:31:03 +0200

    Update TestSWithException.java

--- test/java/spring/TestSWithException.java
+++ test/java/spring/TestSWithException.java
@@ -12,58 +12,87 @@ import static org.junit.Assert.*;
 public class TestSWithException {
     AnnotationConfigApplicationContext context;
 
-    @Test(expected = NullPointerException.class)
-    public void testWithoutAutowired() {
+    @Test
+    public void testWithoutAutowiredInMyApp() {
         context = new AnnotationConfigApplicationContext(Init.class);
         MyApplication app = context.getBean(MyApplication.class);
-        app.processA("something");
+
+        Exception e = assertThrows(NullPointerException.class, () -> app.processA("Hi"));
+
+        assertTrue(e.getMessage().contains("Cannot invoke \"spring.A.msg(String)\" because \"this.classA\" is null"));
     }
 
-    @Test(expected = UnsatisfiedDependencyException.class)
+    @Test
     public void testWithoutBean() {
-        context = new AnnotationConfigApplicationContext(Init.class);
-        MyApplication app = context.getBean(MyApplication.class);
-        app.processA("something");
-    }
-
-    @Test(expected = NoSuchBeanDefinitionException.class)
-    public void testWithoutComponentScan() {
-        context = new AnnotationConfigApplicationContext(Init.class);
-        MyApplication app = context.getBean(MyApplication.class);
-        app.processA("something");
-    }
-
-    @Test(expected = UnsatisfiedDependencyException.class)
-    public void testWithoutQualifierInInit() {
-        context = new AnnotationConfigApplicationContext(Init.class);
-        MyApplication app = context.getBean(MyApplication.class);
-        app.processA("something");
-    }
-
-    @Test(expected = UnsatisfiedDependencyException.class)
-    public void testWithoutQualifierInMyApp() {
-        context = new AnnotationConfigApplicationContext(Init.class);
-        MyApplication app = context.getBean(MyApplication.class);
-        app.processA("something");
-    }
-
-    @Test
-    public void testWrongFieldTypeClassB() {
         Exception e = assertThrows(UnsatisfiedDependencyException.class, () -> context = new AnnotationConfigApplicationContext(Init.class));
-        //context = new AnnotationConfigApplicationContext(Init.class);
-        assertTrue(e.getMessage().contains("Unsatisfied dependency expressed through field 'classB':"));
-    }
 
-    @Test
-    public void testWrongFieldTypeClassA() {
-        Exception e = assertThrows(UnsatisfiedDependencyException.class, () -> context = new AnnotationConfigApplicationContext(Init.class));
-        //context = new AnnotationConfigApplicationContext(Init.class);
         assertTrue(e.getMessage().contains("Error creating bean with name 'myApplication': Unsatisfied dependency expressed through field 'classA':"));
     }
+
+    @Test
+    public void testWithoutComponentScan() {
+        context = new AnnotationConfigApplicationContext(Init.class);
+
+        Exception e = assertThrows(NoSuchBeanDefinitionException.class, () -> context.getBean(MyApplication.class));
+
+        assertTrue(e.getMessage().contains("No qualifying bean of type 'spring.MyApplication' available"));
+    }
+
+    @Test
+    public void testWithoutComponent() {
+        context = new AnnotationConfigApplicationContext(Init.class);
+
+        Exception e = assertThrows(NoSuchBeanDefinitionException.class, () -> context.getBean(MyApplication.class));
+
+        assertTrue(e.getMessage().contains("No qualifying bean of type 'spring.MyApplication' available"));
+    }
+
+    @Test
+    public void testWithoutQualifierInInit() {
+        Exception e = assertThrows(UnsatisfiedDependencyException.class, () -> context = new AnnotationConfigApplicationContext(Init.class));
+
+        assertTrue(e.getMessage().contains("Error creating bean with name 'myApplication': Unsatisfied dependency expressed through field 'classD':"));
+    }
+
+    @Test
+    public void testWithoutQualifierInMyApp() {
+        Exception e = assertThrows(UnsatisfiedDependencyException.class, () -> context = new AnnotationConfigApplicationContext(Init.class));
+
+        assertTrue(e.getMessage().contains("Error creating bean with name 'myApplication': Unsatisfied dependency expressed through field 'classD':"));
+    }
+
+    @Test
+    public void testWrongFieldTypeClassD() {
+        Exception e = assertThrows(UnsatisfiedDependencyException.class, () -> context = new AnnotationConfigApplicationContext(Init.class));
+
+        assertTrue(e.getMessage().contains("Error creating bean with name 'myApplication': Unsatisfied dependency expressed through field 'classD':"));
+    }
+
     @Test
     public void testPassInterfaceToConstructor() {
         Exception e = assertThrows(BeanCreationException.class, () -> context = new AnnotationConfigApplicationContext(Init.class, AI.class));
-        //context = new AnnotationConfigApplicationContext(Init.class);
+
         assertTrue(e.getMessage().contains("Error creating bean with name 'AI': Failed to instantiate [spring.AI]: Specified class is an interface"));
+    }
+
+    @Test
+    public void testWithoutAutowiredInClassDeclaration() {
+        context = new AnnotationConfigApplicationContext(Init.class);
+        MyApplication app = context.getBean(MyApplication.class);
+
+        A classA = app.classB.aField;
+        assertNull(classA);
+    }
+
+    @Test
+    public void testWithoutAutowiredInClassDeclarationCClass() {
+        context = new AnnotationConfigApplicationContext(Init.class);
+        MyApplication app = context.getBean(MyApplication.class);
+
+        B classB = app.classB;
+        B classBInC = app.classC.bField;
+
+        assertNotNull(classB);
+        assertNull(classBInC);
     }
 }
