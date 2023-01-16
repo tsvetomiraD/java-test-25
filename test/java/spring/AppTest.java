ad2a7e69412302d2e09f73dcf62fa0d69a07757b
tsvetomiraD <95402102+tsvetomiraD@users.noreply.github.com>
2023-01-16 10:30:46 +0200

    Update AppTest.java

--- test/java/spring/AppTest.java
+++ test/java/spring/AppTest.java
@@ -19,9 +19,17 @@ public class AppTest
     }
 
     @Test
+    public void testGetBeanNotNull() {
+        MyApplication app = context.getBean(MyApplication.class);
+
+        assertNotNull(app);
+    }
+    @Test
     public void testTwoInstancesMyApplication() {
         MyApplication app = context.getBean(MyApplication.class);
         MyApplication app2 = context.getBean(MyApplication.class);
+
+        assertNotNull(app);
         assertEquals(app, app2);
     }
 
@@ -31,21 +39,56 @@ public class AppTest
         AI a1 = app.classA;
         MyApplication app2 = context.getBean(MyApplication.class);
         AI a2 = app2.classA;
+
+        assertNotNull(a1);
         assertEquals(a1, a2);
     }
 
     @Test
     public void testQualifier() {
         MyApplication app = context.getBean(MyApplication.class);
-        AI a = app.classA;
-        AI b = app.classB;
-        assertNotEquals(a, b);
+        DI d = app.classD;
+        DI e = app.classE;
+
+        assertNotNull(d);
+        assertNotNull(e);
+        assertNotEquals(d, e);
     }
 
     @Test
     public void testRightMsg() {
         MyApplication app = context.getBean(MyApplication.class);
-        String expected = "Message send for A class: Hi Pankaj";
-        assertEquals(expected, app.processA("Hi Pankaj"));
+        String expected = "Message send for A class: Hi";
+
+        assertEquals(expected, app.processA("Hi"));
+    }
+
+    @Test
+    public void testAfieldInBClassNotNull() {
+        MyApplication app = context.getBean(MyApplication.class);
+        A classAInB = app.classB.aField;
+
+        assertNotNull(classAInB);
+    }
+
+    @Test
+    public void testAfieldInBClassEqualsWithAClass() {
+        MyApplication app = context.getBean(MyApplication.class);
+        A classA = app.classA;
+        A classAInB = app.classB.aField;
+
+        assertNotNull(classAInB);
+        assertEquals(classA, classAInB);
+    }
+
+    @Test
+    public void testClassFieldClassWithFieldClass() {
+        MyApplication app = context.getBean(MyApplication.class);
+        C classC = app.classC;
+        A classA = app.classA;
+        A classAInBInC = classC.bField.aField;
+
+        assertNotNull(classAInBInC);
+        assertEquals(classA, classAInBInC);
     }
 }
